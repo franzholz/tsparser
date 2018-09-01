bf00847951a39c3c5eeeca54e5bf2f009a712fc2
Franz Holzinger <holzinger@ttproducts.de>
2018-09-01 15:57:00 +0200

    convert TAB into 4 blanks. Next version 0.3.1

--- ext_emconf.php
+++ ext_emconf.php
@@ -6,37 +6,37 @@
 
 
 $EM_CONF[$_EXTKEY] = array(
-	'title' => 'More Constants Editor Types',
-	'description' => 'This extension adds more types to the Constants Editor. This is a patch for TYPO3.',
-	'category' => 'be',
-	'author' => 'Franz Holzinger',
-	'author_email' => 'franz@ttproducts.de',
-	'shy' => '',
-	'dependencies' => '',
-	'conflicts' => '',
-	'priority' => '',
-	'module' => '',
-	'state' => 'stable',
-	'internal' => '',
-	'uploadfolder' => 0,
-	'createDirs' => '',
-	'modify_tables' => '',
-	'clearCacheOnLoad' => 0,
-	'lockType' => '',
-	'author_company' => 'jambage.com',
-	'version' => '0.3.0',
-	'constraints' => array(
-		'depends' => array(
-			'php' => '5.3.0-7.99.99',
-			'typo3' => '6.1.0-8.99.99',
-		),
-		'conflicts' => array(
-		),
-		'suggests' => array(
-		),
-	),
-	'_md5_values_when_last_written' => 'a:6:{s:9:"ChangeLog";s:4:"44da";s:12:"ext_icon.gif";s:4:"4d2b";s:17:"ext_localconf.php";s:4:"8a72";s:10:"README.txt";s:4:"ee2d";s:14:"doc/manual.sxw";s:4:"c75d";s:40:"xclass/class.user_t3lib_tsparser_ext.php";s:4:"a578";}',
-	'suggests' => array(
-	),
+    'title' => 'More Constants Editor Types',
+    'description' => 'This extension adds more types to the Constants Editor. This is a patch for TYPO3.',
+    'category' => 'be',
+    'author' => 'Franz Holzinger',
+    'author_email' => 'franz@ttproducts.de',
+    'shy' => '',
+    'dependencies' => '',
+    'conflicts' => '',
+    'priority' => '',
+    'module' => '',
+    'state' => 'stable',
+    'internal' => '',
+    'uploadfolder' => 0,
+    'createDirs' => '',
+    'modify_tables' => '',
+    'clearCacheOnLoad' => 0,
+    'lockType' => '',
+    'author_company' => 'jambage.com',
+    'version' => '0.3.1',
+    'constraints' => array(
+        'depends' => array(
+            'php' => '5.3.0-7.99.99',
+            'typo3' => '6.1.0-8.99.99',
+        ),
+        'conflicts' => array(
+        ),
+        'suggests' => array(
+        ),
+    ),
+    '_md5_values_when_last_written' => 'a:6:{s:9:"ChangeLog";s:4:"44da";s:12:"ext_icon.gif";s:4:"4d2b";s:17:"ext_localconf.php";s:4:"8a72";s:10:"README.txt";s:4:"ee2d";s:14:"doc/manual.sxw";s:4:"c75d";s:40:"xclass/class.user_t3lib_tsparser_ext.php";s:4:"a578";}',
+    'suggests' => array(
+    ),
 );
 
