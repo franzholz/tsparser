9e9975e11d5c37adbfde4c14ab252889028f1bb6
Franz Holzinger <holzinger@ttproducts.de>
2021-05-19 12:09:54 +0200

    composer

--- ext_emconf.php
+++ ext_emconf.php
@@ -5,27 +5,25 @@
  ***************************************************************/
 
 
-$EM_CONF[$_EXTKEY] = array(
+$EM_CONF[$_EXTKEY] = [
     'title' => 'More Constants Editor Types',
     'description' => 'This extension adds more types to the Constants Editor. This is a patch for TYPO3.',
     'category' => 'be',
     'author' => 'Franz Holzinger',
     'author_email' => 'franz@ttproducts.de',
     'state' => 'stable',
-    'uploadfolder' => 0,
-    'createDirs' => '',
     'clearCacheOnLoad' => 0,
     'author_company' => 'jambage.com',
-    'version' => '0.3.2',
-    'constraints' => array(
-        'depends' => array(
+    'version' => '0.3.3',
+    'constraints' => [
+        'depends' => [
             'php' => '5.5.0-7.99.99',
             'typo3' => '8.7.0-10.4.99',
-        ),
-        'conflicts' => array(
-        ),
-        'suggests' => array(
-        ),
-    ),
-);
+        ],
+        'conflicts' => [
+        ],
+        'suggests' => [
+        ],
+    ],
+];
 
