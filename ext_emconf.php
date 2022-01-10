3d5cbf2a316af07c81c34b054228f5b9431f53bd
Franz Holzinger <franz.holzinger@festwein.de>
2022-01-10 18:56:26 +0100

    * ready for PHP 8#

--- ext_emconf.php
+++ ext_emconf.php
@@ -14,10 +14,10 @@ $EM_CONF[$_EXTKEY] = [
     'state' => 'stable',
     'clearCacheOnLoad' => 0,
     'author_company' => 'jambage.com',
-    'version' => '0.3.4',
+    'version' => '0.4.0',
     'constraints' => [
         'depends' => [
-            'php' => '7.2.0-7.99.99',
+            'php' => '7.2.0-8.1.99',
             'typo3' => '8.7.0-11.5.99',
         ],
         'conflicts' => [
